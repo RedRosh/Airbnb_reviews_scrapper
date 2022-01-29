c8c73c851ec73c96c170c5b6d7afa85d890c7214
RedRosh <filali_rochdi_@hotmail.fr>
2022-01-29 03:22:42 +0100

    accepting the url from command line as arg

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const URL =
   "https://www.airbnb.com/rooms/44477561?adults=2&check_in=2022-01-27&check_out=2022-01-29&previous_page_section_name=1000&federated_search_id=2623f5af-f921-4aaf-b1cf-00597ec40d3f&guests=1";
 
 main = async () => {
-  const airbnbScrapper = new AirbnbScrapper(URL);
+  let url = process.argv.slice(2)[0] || URL;
+  const airbnbScrapper = new AirbnbScrapper(url);
   await airbnbScrapper.getData();
 };
 
